38b4090ea34d65fd9915e18f08669561984c2b6e
Jake Luer <jake.luer@gmail.com>
2011-10-08 18:42:43 -0400

    client js debug

--- template/assets/js/main.js
+++ template/assets/js/main.js
@@ -1,3 +1,21 @@
+/**
+ * Debug Tools from HTML5 Boilerplate
+ */
+window.log = function(){
+  log.history = log.history || [];   // store logs to an array for reference
+  log.history.push(arguments);
+  if(this.console) {
+    arguments.callee = arguments.callee.caller;
+    var newarr = [].slice.call(arguments);
+    (typeof console.log === 'object' ? log.apply.call(console.log, console, newarr) : console.log.apply(console, newarr));
+  }
+};
+
+// make it safe to use console.log always
+(function(b){function c(){}for(var d="assert,clear,count,debug,dir,dirxml,error,exception,firebug,group,groupCollapsed,groupEnd,info,log,memoryProfile,memoryProfileEnd,profile,profileEnd,table,time,timeEnd,timeStamp,trace,warn".split(","),a;a=d.pop();){b[a]=b[a]||c}})((function(){try
+{console.log();return window.console;}catch(err){return window.console={};}})());
+
+
 $(document).ready(function() {
   // :D
 });
