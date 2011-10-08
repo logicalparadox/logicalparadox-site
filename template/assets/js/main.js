ad4beeabc558452f5fb6d79fb590b7a239e3994e
Jake Luer <jake.luer@gmail.com>
2011-10-08 18:43:28 -0400

    slider template updates supports page init

--- template/assets/js/main.js
+++ template/assets/js/main.js
@@ -16,6 +16,79 @@ window.log = function(){
 {console.log();return window.console;}catch(err){return window.console={};}})());
 
 
+
+/**
+ * Theme custom slider jQuery
+ * 
+ * @param {Object} options
+ */
+
+if (typeof jQuery != 'undefined') {
+  jQuery(function($) {
+    $.fn.extend({
+      slider: function(options) {
+        var settings = $.extend({}, $.fn.slider.defaults, options);
+        return this.each(function() {
+          var $$ = $(this)
+            , o = $.metadata ? $.extend({}, settings, $$.metadata()) : settings;
+          
+          var $blocks = $('.blocks', $$)
+            , $controls = $('.controls', $$)
+            , $panels = $('.panels', $$);
+          
+          $blocks.css({ display: 'none' });
+          
+          $('.block', $blocks).each(function (i) {
+            $(this).attr('block-index', i);
+          });
+          
+          var rotate = function (last) {
+            var len = $('.block', $blocks).length - 1
+              , $curr
+              , count = 0;
+            
+            for (var i = 0; i < settings.blocks; i ++) {
+              if (last > len) last = 0;
+              
+              $curr = $('.block', $blocks).filter(function () {
+                return $(this).attr('block-index') == last;
+              })[0];
+              
+              $($curr).clone().appendTo($panels);
+              
+              log($curr, last);
+              
+              last++;
+            }
+            
+            setTimeout(function() {
+              //rotate(last);
+            }, 5000);
+            
+          }
+          
+          log($blocks, $controls, $panels);
+          rotate(0);
+        });
+      }
+    });
+    /**
+     * Slider Defaults
+     */
+    $.fn.slider.defaults = {
+      blocks: 4
+    };
+  }(jQuery));
+}
+
+
+/**
+ * Theme custom slider
+ */
+
 $(document).ready(function() {
-  // :D
+  
+  $('.widget#code').slider();
+  
+  
 });
